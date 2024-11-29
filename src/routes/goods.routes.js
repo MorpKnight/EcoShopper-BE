ec8e0d34a3095ab5cf95a25354c5c8c07d211fb7
MorpKnight <christoffelsihombing@gmail.com>
2024-11-29 22:07:57 +0700

    Update goods routes to use route parameters for better resource identification

--- src/routes/goods.routes.js
+++ src/routes/goods.routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const goodsControllers = require('../controllers/goods.controllers');
 
 router.get('/', goodsControllers.getGoods);
-router.get('/good', goodsControllers.getGood);
-router.get('/category', goodsControllers.getGoodsByCategory);
-router.get('/name', goodsControllers.getGoodsByName);
-router.get('/sr', goodsControllers.getGoodsBySR);
-router.get('/producer', goodsControllers.getGoodsByProducer);
-router.get('/alternative', goodsControllers.goodsAlternative);
+router.get('/good/:id', goodsControllers.getGood);
+router.get('/category/:category', goodsControllers.getGoodsByCategory);
+router.get('/name/:name', goodsControllers.getGoodsByName);
+router.get('/sr/:sr', goodsControllers.getGoodsBySR);
+router.get('/producer/:producer', goodsControllers.getGoodsByProducer);
+router.get('/alternative/:alternative', goodsControllers.goodsAlternative);
 
 module.exports = router;
