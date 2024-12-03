3d03208982f25a7298328c4780c34f5b46c75e79
MorpKnight <christoffelsihombing@gmail.com>
2024-12-04 00:51:54 +0700

    Fix token extraction in middleware, update goods routes for better clarity, and enhance logging for user profile fetches

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -2,9 +2,8 @@ const jwt = require('jsonwebtoken');
 const { pool } = require('../config/db.config');
 
 exports.verifyToken = async (req, res, next) => {
-    console.log(req.headers);
     try {
-        const token = req.headers.cookie.split('=')[1];
+        const token = req.headers.cookies.split('=')[1];
         if (!token) throw new Error('Token not found');
         
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
--- src/routes/goods.routes.js
+++ src/routes/goods.routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const goodsControllers = require('../controllers/goods.controllers');
 
-router.get('/', goodsControllers.getGoods);
-router.get('/good/:id', goodsControllers.getGood);
+router.get('/alternative/:alternative', goodsControllers.goodsAlternative);
 router.get('/category/:category', goodsControllers.getGoodsByCategory);
 router.get('/name/:name', goodsControllers.getGoodsByName);
 router.get('/sr/:sr', goodsControllers.getGoodsBySR);
 router.get('/producer/:producer', goodsControllers.getGoodsByProducer);
-router.get('/alternative/:alternative', goodsControllers.goodsAlternative);
+router.get('/:id', goodsControllers.getGood);
+router.get('/', goodsControllers.getGoods);
 
 module.exports = router;
